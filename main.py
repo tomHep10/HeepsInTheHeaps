f192b6cbee97d9aa006aa226d9c1819b11896e6f
ThomasHeeps <thomasheeps10@gmail.com>
2025-04-16 18:25:43 -0400

    Created initial main function to interact with the pathfinding class

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    input = int(("1 for a*, 2 for d*: "))
+
+    if input == 1:
+        pass
+    if input == 2:
+        pass
+    else:
+        print("Invalid input, please try again.")
+        main()
