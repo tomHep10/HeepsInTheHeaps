bcc060cf90cd19fa9b46830a11a473795ea4d835
ThomasHeeps <thomasheeps10@gmail.com>
2025-04-17 17:00:28 -0400

    Created initial graph for a* algo and updated input in main for easier debugging

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-def main():
-    input = int(("1 for a*, 2 for d*: "))
+from pathGraph import pathGraph
 
-    if input == 1:
-        pass
-    if input == 2:
-        pass
+def main():
+    i = int(input("1 for a*, 2 for d*, 0 for exit: "))
+    graph = pathGraph()
+
+    if i == 0:
+        exit()
+    if i == 1:
+        graph._createAStarGraph(20, 20)
+        main()
+    if i == 2:
+        main()
     else:
         print("Invalid input, please try again.")
         main()
+
+if __name__ == "__main__":
+    main()
--- pathGraph.py
+++ pathGraph.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 class Node:
    def __init__(self):
@@ -9,13 +10,23 @@ class Node:
         self.parent_col = 0 # parents location in col
 
 class pathGraph:
-
   def __init__(self):
     self.aStarGraph = None
     self.dStarGraph = None
 
-  def _createAStarGraph(self):
-    pass
+  def _createAStarGraph(self, row, col):
+    grid = np.zeros((row, col)) # 317x317 grid
+    probabilityOfObstacle = 0.3 # 30% chance of an obstacle
+
+    # adding obstacles to the grid
+    for i in range(row):
+      for j in range(col):
+        if random.random() < probabilityOfObstacle:
+          grid[i][j] = 1
+
+    # printing out grid
+    for row in grid:
+        print(' '.join(str(cell) for cell in row))
 
   def _createDStarGraph(self):
     pass
