6925b56476ec508eb247952e59ad6e547c7b84da
phill <phillipslogan04@gmail.com>
2025-04-22 23:22:30 -0400

    Fixed issue with A* graph plotting

--- pathGraph.py
+++ pathGraph.py
@@ -224,7 +224,7 @@ class pathGraph:
 		g = np.array(self.grid, dtype=float)
 		row, col = g.shape
 
-		flipped_path = [(row - 1 - r, c) for r, c in self.aStarPath]
+		flipped_path = [(r, col-1-c) for r, c in self.dstar.getPath()]
 		for i in range(row):
 			for j in range(col):
 				g[i][j] = 0.3 if g[i][j] == 1 else 0.0
@@ -240,15 +240,15 @@ class pathGraph:
 		ax.grid(which='both', color='black', linestyle='-', linewidth=1)
 		ax.imshow(g, cmap="Greys", extent=[0, row, 0, col], vmin=0, vmax=1)
 
-		flipped_path = self.dstar.getPath()
+		#flipped_path = self.dstar.getPath()
 		if flipped_path:
 			x_values = [x + 0.5 for x, y in flipped_path]
 			y_values = [y + 0.5 for x, y in flipped_path]
 			ax.plot(x_values, y_values, color='red', linewidth=2, marker='o', markersize=5)
 
 
-		sx, sy = 0+0.5, (row - 1) + 0.5
-		gx, gy = (col-1) + 0.5, 0 + 0.5
+		sx, sy = 0, (row - 1)
+		gx, gy = (col-1), 0
 		ax.plot(sx + 0.5, sy + 0.5, color='green', marker='o', markersize=10)
 		ax.text(sx + 0.5, sy + 0.5, 'Start', color='green', fontsize=10, ha='left', va='bottom')
 
